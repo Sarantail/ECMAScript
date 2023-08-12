ff7c14b8076c253cc66efb7c5abd92c614d8f3a4
Ann <102371972+Sarantail@users.noreply.github.com>
2023-08-12 10:54:10 +0300

    Update script.js

--- 2/script.js
+++ 2/script.js
@@ -6,20 +6,18 @@
 // Свойство author (автор) - строка, имя автора книги.
 // Свойство grade (количество страниц) - число, количество страниц в книге.
 // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
-        class Book {
-            constructor(title, author, grade) {
-                this.title = title;
-                this.author = author;
-                this.grade = grade;
-            }
-            displayInfo() {
-                console.log(book.title); 
-                console.log(book.author); 
-                console.log(book.grade); 
-            }
-        }
+class Book {
+    constructor(title, author, grade) {
+        this.title = title;
+        this.author = author;
+        this.grade = grade;
+    }
+    displayInfo() {
+        console.log(book.title, book.author, book.grade); 
+    }
+}
 
-        let book = new Book('Евгений Онегин', 'А.С.Пушкин', 288);
+const book = new Book('Евгений Онегин', 'А.С.Пушкин', 288);
 
 
 
@@ -30,17 +28,15 @@
 // Свойство age (возраст) - число, возраст студента.
 // Свойство grade (класс) - строка, класс, в котором учится студент.
 // Метод displayInfo() - выводит информацию о студенте в консоль.
-        class Student {
-            constructor(name, age, grade) {
-                this.name = name;
-                this.age = age;
-                this.grade = grade;
-            }
-            displayInfo() {
-                console.log(book.name); 
-                console.log(book.age); 
-                console.log(book.grade); 
-            }
+class Student {
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    displayInfo() {
+        console.log(book.name, book.age, book.grade); 
+    }
         }
 
-        let student = new Student('Вова', 20, 2);
+const student = new Student('Вова', 20, 2);
